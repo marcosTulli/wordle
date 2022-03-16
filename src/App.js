08a8b26631c33d7fb723cc556c83398b29cc3cbe
marcosTulli <marcostulli@hotmail.com>
2022-03-16 16:49:45 -0300

    moved state management to app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import React, { useState, createContext } from "react";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./Words";
+
+export const AppContext = createContext();
 
 function App() {
-  return <div className="App"></div>;
+  const [board, setBoard] = useState(boardDefault);
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
+    </div>
+  );
 }
 
 export default App;
