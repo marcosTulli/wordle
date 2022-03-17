08765fd6497e8b3f80965248198fc6a45bb9d733
marcosTulli <marcostulli@hotmail.com>
2022-03-17 11:53:06 -0300

    Started building keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Keyboard() {
-  return <div>Keyboard</div>;
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "Q", "M"];
+  return (
+    <div className="keyoard">
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+    </div>
+  );
 }
 
 export default Keyboard;
